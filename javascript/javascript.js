9c5e33f1cf1819e98fcb5c913c82b881875620f8
HOANGHUYHOANG <hoanghuyhoang619@gmail.com>
2022-04-25 15:36:28 +0700

    Initial commit

---
+++ javascript/javascript.js
@@ -0,0 +1,7 @@
+// var a = document.getElementById("math");
+// a.addEventListener('mouseover', function() {
+//     document.getElementById("modal").style.display = "block";
+// });
+// a.addEventListener('mouseout', function() {
+//     document.getElementById("modal").style.display = "none";
+// });
